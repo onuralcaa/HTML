a4858585c24a76ac61b3bcf62fea067a38007c28
Onur ALACA <onuralcaa@gmail.com>
2023-12-22 19:49:55 +0300

    Authentication

---
+++ w12_04_Authentication.php
@@ -0,0 +1,17 @@
+<?php 
+	
+	if(isset($_SERVER['PHP_AUTH_USER']) &&
+	   isset($_SERVER['PHP_AUTH_PW']))
+	{
+		echo "Welcome User: " . $_SERVER['PHP_AUTH_USER'] .
+			  "Password : "   . $_SERVER['PHP_AUTH_PW'];
+	}
+
+	else 
+	{
+		//Burasi kalip
+		header('WWW-Authenticate: Basic realm="Restricted Area"');
+		header('HTTP/1.0 401 Unauthorized');
+		die("Please enter your username and password");
+	}
+?>
