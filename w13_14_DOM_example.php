88cffd89c4dd85ce101e7b5c8f4e85b6915099d4
Onur ALACA <onuralcaa@gmail.com>
2023-12-26 21:49:38 +0300

    DOM example

---
+++ w13_14_DOM_example.php
@@ -0,0 +1,12 @@
+<html>
+	<head>
+		<title>Link Test</title>
+	</head>
+	<body>
+		<a id="mylink" href="http://alacamaker.com">Click me for the website</a><br>
+		<script>
+			url = document.links.mylink.href //url = mylink.href de olur
+			document.write('The URL is ' + url)
+		</script>
+	</body>
+</html>
