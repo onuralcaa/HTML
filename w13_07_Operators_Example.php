a64b1e84ad2dc0c12718533a0924a40c02f36dfa
Onur ALACA <onuralcaa@gmail.com>
2023-12-26 20:31:13 +0300

    Operators

---
+++ w13_07_Operators_Example.php
@@ -0,0 +1,11 @@
+<html>
+  <head><title>Hello World</title></head>
+  <body>
+    <script type="text/javascript"> //eski tarayıcılar için.
+      document.write(12+5)
+    </script>
+    <noscript>
+       Your browser doesn't support or has disabled JavaScript
+    </noscript>
+   </body>
+</html>
