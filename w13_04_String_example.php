d37cb2abdd7e8cdc3de93dfe6dc86b68593e7313
Onur ALACA <onuralcaa@gmail.com>
2023-12-26 20:17:46 +0300

    String example

---
+++ w13_04_String_example.php
@@ -0,0 +1,13 @@
+<html>
+  <body>
+    <script type="text/javascript"> //eski tarayıcılar için.
+
+      greeting = "\"Hello there\" is a greeting"
+
+      document.write(greeting)
+    </script>
+    <noscript>
+       Your browser doesn't support or has disabled JavaScript
+    </noscript>
+   </body>
+</html>
