58c5071927542bb7ae2a73bfebdfe4a07de1ed67
Onur ALACA <onuralcaa@gmail.com>
2023-12-26 20:44:22 +0300

    Escape characters

---
+++ w13_10_Escape_characters.php
@@ -0,0 +1,15 @@
+<html>
+  <body>
+    <pre> //Kaçış taglarının tarayıcı tarafından okunmasını sağlar.
+    <script type="text/javascript"> //eski tarayıcılar için.
+
+    heading = "Name\tAge\tOnur"
+
+      document.write(heading)
+    </script>
+    <noscript>
+       Your browser doesn't support or has disabled JavaScript
+    </noscript>
+  </pre>
+   </body>
+</html>
