060367ca72547cd5e3e2d9ea6178bc6850176279
Onur ALACA <onuralcaa@gmail.com>
2023-12-26 20:39:38 +0300

    String concetantion

---
+++ w13_08_String_concatenation_example.php
@@ -0,0 +1,15 @@
+<html>
+  <head><title>Hello World</title></head>
+  <body>
+    <script type="text/javascript"> //eski tarayıcılar için.
+
+    messages = 3 // ; ile bitip bitmemesi önemli değil. Ancak aynı satırda birden fazla ifadel kullanılırsa ifadeleri birbirinden ayırmak için ; kullanılır. Son komuta koyulmasına da gerek yoktur.
+
+    document.write("You have " + messages + " messages.")  
+
+</script>
+    <noscript>
+       Your browser doesn't support or has disabled JavaScript
+    </noscript>
+   </body>
+</html>
