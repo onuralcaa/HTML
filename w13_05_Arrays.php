298a607ceb5e112be77c6d050053ab7403b9d0b5
Onur ALACA <onuralcaa@gmail.com>
2023-12-26 20:26:36 +0300

    Arrays

---
+++ w13_05_Arrays.php
@@ -0,0 +1,21 @@
+Arrays tanımlamaları
+
+• toys = ['bat', 'ball', 'whistle',
+'puzzle', 'doll']
+
+• face =
+[
+['R', 'G', 'Y'],
+['W', 'R', 'O'],
+['Y', 'W', 'G']
+]
+
+• face = [['R', 'G', 'Y'], ['W', 'R', 'O'],
+['Y', 'W', 'G']]
+
+• top = ['R', 'G', 'Y']
+mid = ['W', 'R', 'O']
+bot = ['Y', 'W', 'G']
+face = [top, mid, bot]
+
+• document.write(face[1][2])
