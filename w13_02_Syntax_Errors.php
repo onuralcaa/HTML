7dbcf2e8db86b5fa4f30ba8906bd029fc1aac530
Onur ALACA <onuralcaa@gmail.com>
2023-12-26 20:14:29 +0300

    Syntax errors

---
+++ w13_02_Syntax_Errors.php
@@ -0,0 +1,10 @@
+
+<html>
+  <head><title>Hello World</title></head>
+  <body>
+    <script type="text/javascript">
+    	//Hata yapıldığı zaman ekrana herhangi uyarı gelmez. Hatayı tarayıcı üzerinden tespit edebiliriz.
+       document.write("Hello World")
+    </script>
+  </body>
+</html>
