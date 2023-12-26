8d3d2944ce263bbf33c03a47580c74ff366a987f
Onur ALACA <onuralcaa@gmail.com>
2023-12-26 20:40:05 +0300

    String example 2

---
+++ w13_09_String_example_2.php
@@ -0,0 +1,18 @@
+<html>
+  <head><title>Hello World</title></head>
+  <body>
+    <script type="text/javascript"> //eski tarayıcılar için.
+
+    //string birleştirme
+
+    name = "James"
+    name += " Dean"
+
+    document.write(name)  
+
+</script>
+    <noscript>
+       Your browser doesn't support or has disabled JavaScript
+    </noscript>
+   </body>
+</html>
