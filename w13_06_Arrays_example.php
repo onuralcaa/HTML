396c20ecdf5fa3e26f3578e7fe798b556bd6ab61
Onur ALACA <onuralcaa@gmail.com>
2023-12-26 20:29:37 +0300

    Arrays example

---
+++ w13_06_Arrays_example.php
@@ -0,0 +1,15 @@
+<html>
+  <head><title>Hello World</title></head>
+  <body>
+    <script type="text/javascript"> //eski tarayıcılar için.
+
+     face = [['R', 'G', 'Y'], ['W', 'R', 'O'],
+['Y', 'W', 'G']]
+
+      document.write(face[0][1])
+    </script>
+    <noscript>
+       Your browser doesn't support or has disabled JavaScript
+    </noscript>
+   </body>
+</html>
